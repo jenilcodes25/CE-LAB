29f696efd079e8ff5484d6228ef7c4593827c54f
Jenil <167628607+jenilcodes25@users.noreply.github.com>
2026-04-22 08:39:40 +0530

    Refactor optimizer for common subexpression elimination

--- EXP10_COMMON_SUBEXPRESSION/cse.py
+++ EXP10_COMMON_SUBEXPRESSION/cse.py
@@ -1,54 +1,42 @@
 def optimizer(expressions):
-    value_table = {}
+    seen = {}
 
     for i, (target, expr) in enumerate(expressions):
-        if ' ' in expr:
-            L, op, R = expr.split(' ')
+        parts = expr.split()
 
-            if L.isdigit() and R.isdigit():
-                # Constant expression evaluation
-                if op == '+':
-                    expressions[i] = (target, int(L) + int(R))
-                elif op == '-':
-                    expressions[i] = (target, int(L) - int(R))
-                elif op == '*':
-                    expressions[i] = (target, int(L) * int(R))
-                elif op == '/':
-                    expressions[i] = (target, int(L) // int(R))
+        # If expression has operator
+        if len(parts) == 3:
+            a, op, b = parts
+
+            # Constant folding
+            if a.isdigit() and b.isdigit():
+                expressions[i] = (target, eval(expr))
 
             else:
-                # Handle variables (subexpression reuse)
-                L_value = value_table.get(L, ord(L))
-                R_value = value_table.get(R, ord(R))
+                # Same key for x+y and y+x
+                key = (op, tuple(sorted([a, b])))
 
-                # Create hash_key to detect common subexpressions
-                hash_key = (op, min(L_value, R_value), max(L_value, R_value))
-
-                if hash_key in value_table:
-                    # Replace with previously computed variable (e.g., t2 = t1)
-                    expressions[i] = (target, expressions[value_table[hash_key]][0])
+                if key in seen:
+                    expressions[i] = (target, seen[key])
                 else:
-                    value_table[hash_key] = i
-
-        # Update variable lookup table
-        value_table[target] = i
+                    seen[key] = target
 
     return expressions
 
 
-# Input expressions
+# Input
 expressions = [
     ('t1', 'x + y'),
     ('t2', 'y + x'),
     ('t3', '8 + 5'),
     ('t4', 'x + z'),
     ('t5', '6 - 3'),
-    ('t5', '5 * 3'),
+    ('t6', '5 * 3')
 ]
 
-# Run optimizer
-ans = optimizer(expressions)
+# Run
+result = optimizer(expressions)
 
-# Print results
-for target, expr in ans:
-    print(f"{target} = {expr}")
+# Output
+for x, y in result:
+    print(x, "=", y)
